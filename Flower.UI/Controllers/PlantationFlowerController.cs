1ceaf7731e6832d78a8356b8e2736bd0f33dc270
Veronika <Veronika@DESKTOP-S01858F>
2020-04-23 02:03:55 +0300

    Added controllers

---
+++ Flower.UI/Controllers/PlantationFlowerController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flower.UI.Controllers
+{
+    public class PlantationFlowerController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
