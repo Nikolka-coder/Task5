4ae4b0621cc215f43a94af1d7e11a0b298ccd094
Veronika <Veronika@DESKTOP-S01858F>
2020-04-23 00:46:31 +0300

    Added services

---
+++ Flower.BAL/Services/FlowerService.cs
@@ -0,0 +1,11 @@
+﻿using Flower.BAL.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Flower.BAL.Services
+{
+    public class FlowerService : IService
+    {
+    }
+}
